9b01f90c0f824e0112a48e2ab54e1dd9bea83222
shayan razavi <87467935+shayanrazavi@users.noreply.github.com>
2021-08-28 22:25:30 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
-from __future__ import unicode_literals
 from telegram.ext import *
-import youtube_dl
 
-api_key="1866200899:AAETOJGi9gDlBjwXmUsCD61r17wO5MhVkmw"
-print('bot started')
+api_key="1777223953:AAHR0WuIhxYRV6ZbJmOAd_2H8648jtxeG_E"
+print("bot started")
 
 def start_command(update,context):
-    update.message.reply_text('Hi . welcome :)You can use this bot to download the videos you want on YouTube :) All you have to do is enter the URL of the video you want.')
+    update.message.reply_text("type:")
+
 def help_command(update,context):
-    update.message.reply_text('Im busy right now. Solve the problem yourself.')
+    update.message.reply_text("help!!!")
 
 def sample_response(text_input):
-    user_message=str(text_input).lower()
-    ydl_opts = {}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        return ydl.download([user_message])
+    user_message = str(text_input).lower()
+    #HI-->hi
+    if user_message in ("hi","hello"):
+        return ("how are you")
+    return "I dont understand you"
 
 def handle_message(update,context):
-    text=str(update.message.text)
+    text=str(update.message.text)    
     response_text=sample_response(text)
     update.message.reply_text(response_text)
 
@@ -25,9 +25,8 @@ updater=Updater(api_key,use_context=True)
 dp=updater.dispatcher
 
 dp.add_handler(CommandHandler("start",start_command))
-dp.add_handler(CommandHandler("help",help_command))
-dp.add_handler(MessageHandler(Filters.text, handle_message))
-
+dp.add_handler(CommandHandler("help",help_command))    
+dp.add_handler(MessageHandler(Filters.text,handle_message))
 
 updater.start_polling()
 updater.idle()
